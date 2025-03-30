24b3c9904c82fabc6ad2f6343aa3102590815919
TheLoveSnack <adamsl178@gmail.com>
2025-03-30 16:07:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
+import numpy as np
+import pandas as pd
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+combined = pd.read_csv("combined.csv")
+
+cola1, cola2, cola3 = st.columns([1,1,1])
+colb1, colb2, colb3 = st.columns([1,1,1])
+
+leagues = combined['league'].unique()
+league = cola1.selectbox('League',
+leagues.tolist())
+
+years = combined['season'].unique()
+year = colb3.selectbox('Year',
+years.tolist())
+
+filters = combined[(combined['league']==league) & (combined['season']==year)]
+st.write(filters)
